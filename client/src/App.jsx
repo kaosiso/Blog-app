db62dae8a7f7cc39c5e1ce838a93bcfa4fd621ca
kaosiso <kaosisochukwum@gmail.com>
2024-12-05 11:21:23 +0000

    create and add footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Projects from "./pages/Projects"
 import Header from "./components/Header"
-
+import FooterCom from "./components/Footer";
 
 
 
@@ -25,6 +25,7 @@ export default function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/projects" element={<Projects/>}/>
         </Routes>
+        <FooterCom/>
       </BrowserRouter>
     </div>
   );
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 function SignIn() {
-  return <div></div>;
+  return <div>
+    sign in
+  </div>;
 }
 
 export default SignIn;
